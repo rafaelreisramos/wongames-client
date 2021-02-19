a675803cc41fcedf0e58f825635b99bba98f3986
rafaelreisramos <7602616+rafaelreisramos@users.noreply.github.com>
2021-02-19 10:30:08 -0300

    Create Logo component (#3)
    - Add won games svg logo.
    - Add currentColor in svg and null assertion TS.
    - Add args and controls to storybook.
    - Address logo accessibility.
    - Update jest config to support paths from /src.
    - Test rendered logo default color.
    - Add new logo prop size.
    - Hide logo text on mobile.
    - Avoid pointer on logo when text is hidden.
    - Test media queries with styled-media-query on jest.

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
   }
 }
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+import { LogoProps } from '.'
+
+const containerModifiers = {
+  normal: () => css`
+    width: 11rem;
+    height: 3.3rem;
+  `,
+
+  large: () => css`
+    width: 20rem;
+    height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
+  `
+}
+
+export const Container = styled.div<LogoProps>`
+  ${({ theme, color, size, hideOnMobile }) => css`
+    color: ${theme.colors[color!]};
+
+    ${!!size && containerModifiers[size]}
+
+    ${!!hideOnMobile && containerModifiers.hideOnMobile}
+  `}
+`
