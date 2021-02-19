4e424f84e0d135b40a46b9bd70a88d198f16c241
rafaelreisramos <rrramos@gmail.com>
2021-02-19 16:53:32 -0300

    feat: add Heading tests
    Add font-color, border left and border bottom tests.
    Adjust border-bottom spacing and width.
    Replace babel.config.js with .json version once it wasn't working with env key.
    Use env key with ssr/displayName: false to make toHaveStyleRule works.

--- src/components/Heading/index.test.tsx
+++ src/components/Heading/index.test.tsx
@@ -5,11 +5,37 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render a white label by default', () => {
+  it('should render a white heading by default', () => {
     renderWithTheme(<Heading>Most Populars</Heading>)
 
     expect(
       screen.getByRole('heading', { name: /most populars/i })
-    ).toBeInTheDocument()
+    ).toHaveStyle({ color: '#fafafa' })
+  })
+
+  it('should render a black heading when color is passed', () => {
+    renderWithTheme(<Heading color="black">Most Populars</Heading>)
+
+    expect(
+      screen.getByRole('heading', { name: /most populars/i })
+    ).toHaveStyle({ color: '#030517' })
+  })
+
+  it('should render a heading with a left line', () => {
+    renderWithTheme(<Heading lineLeft>Most Populars</Heading>)
+
+    expect(
+      screen.getByRole('heading', { name: /most populars/i })
+    ).toHaveStyle({ 'border-left': '0.7rem solid #3cd3c1' })
+  })
+
+  it('should render a heading with a bottom line', () => {
+    renderWithTheme(<Heading lineBottom>Most Populars</Heading>)
+
+    expect(
+      screen.getByRole('heading', { name: /most populars/i })
+    ).toHaveStyleRule('border-bottom', '0.5rem solid #f231a5', {
+      modifier: '::after'
+    })
   })
 })
