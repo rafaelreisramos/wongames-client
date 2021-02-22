b6c77d6f70a7a71d0cfed69dafdb421c44338fd7
rafaelreisramos <rrramos@gmail.com>
2021-02-22 16:23:11 -0300

    test: test button sizes
    Test button size, paddings and font-size

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -5,11 +5,33 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Button from '.'
 
 describe('<Heading />', () => {
-  it('should render a button by default', () => {
+  it('should render the medium size button by default', () => {
     renderWithTheme(<Button>Button</Button>)
 
     expect(screen.getByRole('button', { name: /button/i })).toHaveStyle({
-      color: '#fafafa'
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
+
+  it('should render the small size button', () => {
+    renderWithTheme(<Button size="small">Button</Button>)
+
+    expect(screen.getByRole('button', { name: /button/i })).toHaveStyle({
+      height: '3rem',
+      padding: '0.8rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render the large size button', () => {
+    renderWithTheme(<Button size="large">Button</Button>)
+
+    expect(screen.getByRole('button', { name: /button/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
     })
   })
 })
