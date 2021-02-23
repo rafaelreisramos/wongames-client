0d46f9d39b8ddf21596241121e80c9533a459fca
rafaelreisramos <7602616+rafaelreisramos@users.noreply.github.com>
2021-02-23 20:14:15 -0300

    feat: Menu component (#11)
    Close #6 
    
    Add Menu component and styles.
    Add tests to verify if the layout meets design requirements.
    Add open/close menu logic and tests and hide/show info when the user is logged and tests.
    Add Storybook parameters to show dark and fullscreen.
    Add menu animations.
    Change jest config file to exclude stories from coverage.
    Adjust menu layout for mobile/desktop accordingly to media passed.

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill'
+import {
+  ShoppingCart as ShoppingCartIcon,
+  Search as SearchIcon,
+  Close as CloseIcon
+} from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+import Logo from 'components/Logo'
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
+
+export type MenuProps = {
+  username?: string
+}
+
+const Menu = ({ username }: MenuProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Container>
+      <MediaMatch lessThan="medium">
+        <S.IconContainer onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconContainer>
+      </MediaMatch>
+
+      <S.LogoContainer>
+        <Logo hideOnMobile />
+      </S.LogoContainer>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
+      <S.MenuGroup>
+        <S.IconContainer>
+          <SearchIcon aria-label="Search" />
+        </S.IconContainer>
+        <S.IconContainer>
+          <ShoppingCartIcon aria-label="Shopping Cart" />
+        </S.IconContainer>
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.SignUp href="#" title="Sign Up">
+              Sign Up
+            </S.SignUp>
+          </S.RegisterBox>
+        )}
+      </S.MenuFull>
+    </S.Container>
+  )
+}
+
+export default Menu
