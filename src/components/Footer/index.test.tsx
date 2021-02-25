9a4d19c26625a87b255164fc7f3a7e78ae15e7eb
rafaelreisramos <rrramos@gmail.com>
2021-02-25 18:03:52 -0300

    feat: add footer component
    Add footer component.
    Add storybook add-on support for next.
    Add footer test to check if all elements have been rendered.

---
+++ src/components/Footer/index.test.tsx
@@ -0,0 +1,33 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Footer from '.'
+
+describe('<Footer />', () => {
+  it('should render the black logo, 4 columns topics and copyright', () => {
+    const { container } = renderWithTheme(<Footer />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /location/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/all rights reserved/i)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
