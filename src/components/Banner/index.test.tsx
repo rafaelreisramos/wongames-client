08b29cd8ae00f7c1b70440df510bbbbd6c2f2232
rafaelreisramos <rrramos@gmail.com>
2021-03-03 16:01:35 -0300

    feat: add Banner component
    Created the Banner component with styles, tests and storybook.

---
+++ src/components/Banner/index.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/random/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render the banner', () => {
+    const { container } = renderWithTheme(<Banner {...props} />)
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
