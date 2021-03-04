7918d396161ccda931137cdc5cd572f838e15dbf
rafaelreisramos <7602616+rafaelreisramos@users.noreply.github.com>
2021-03-04 18:04:33 -0300

    feat: Highlight component (#20)
    Added Highlight component with layout, styles and tests.
    Added component storybook.
    Added optional float image.
    Added left/right text alignment option.
    Added background image and overlay.

---
+++ src/components/Highlight/index.stories.tsx
@@ -0,0 +1,39 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  argTypes: {
+    type: {
+      floatImage: 'string'
+    }
+  },
+  args: {
+    backgroundImage: '/img/red-dead-img.jpg',
+    title: `Read Dead it's back`,
+    subtitle: `Come see John's new adventures`,
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/read-dead'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const withFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+withFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
