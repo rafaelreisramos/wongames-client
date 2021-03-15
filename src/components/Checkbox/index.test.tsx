85b123e9f77afac7a3fd5ca34ddafe039fda5219
rafaelreisramos <rrramos@gmail.com>
2021-03-15 16:16:44 -0300

    feat: add Checkbox component
    Added layout, styles, storybook, and control for Checkbox component.

---
+++ src/components/Checkbox/index.test.tsx
@@ -0,0 +1,74 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with a black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should dispatch onCheck(true) when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="ckeckbox label" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+
+    expect(onCheck).toHaveBeenCalledWith(true)
+  })
+
+  it('should dispatch onCheck(false) when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(
+      <Checkbox label="ckeckbox label" onCheck={onCheck} isChecked />
+    )
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+
+    expect(onCheck).toHaveBeenCalledWith(false)
+  })
+
+  it('should be accessible with tab', async () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(screen.getByLabelText(/checkbox label/i)).toHaveFocus()
+  })
+})
---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,50 @@
+import { InputHTMLAttributes, useState } from 'react'
+import * as S from './styles'
+
+export type CheckboxProps = {
+  label?: string
+  labelFor?: string
+  labelColor?: 'white' | 'black'
+  isChecked?: boolean
+  onCheck?: (status: boolean) => void
+  value?: string | ReadonlyArray<string> | number
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Checkbox = ({
+  label,
+  labelFor = '',
+  labelColor = 'white',
+  onCheck,
+  isChecked = false,
+  value,
+  ...props
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(isChecked)
+
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    !!onCheck && onCheck(status)
+  }
+
+  return (
+    <S.Container>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+        value={value}
+        {...props}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Container>
+  )
+}
+
+export default Checkbox
