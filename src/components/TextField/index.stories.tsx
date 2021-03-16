61f7134704c14d3665c803aa505503695daeb32a
rafaelreisramos <7602616+rafaelreisramos@users.noreply.github.com>
2021-03-16 16:51:26 -0300

    feat: TextField component (#32)
    * feat: add TextField component
    - Added input field.
    - Icon option rendering added.
    - Disable text input and label option.
    - Input error text option.
    - Added styles, tests, and storybook.
    
    * style: add red color to theme

---
+++ src/components/TextField/index.stories.tsx
@@ -0,0 +1,39 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { Email } from '@styled-icons/material-outlined'
+
+import TextField, { TextFieldProps } from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    id: 'email',
+    label: 'E-mail',
+    labelFor: 'email',
+    icon: <Email />,
+    initialValue: '',
+    placeholder: 'john.doe@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'typed text' },
+    icon: {
+      type: ''
+    }
+  }
+} as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops... something is wrong'
+}
---
+++ src/components/TextField/index.test.tsx
@@ -0,0 +1,121 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Email } from '@styled-icons/material-outlined'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import TextField from '.'
+
+describe('<TextField />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<TextField label="label" labelFor="field" id="field" />)
+
+    expect(screen.getByLabelText('label')).toBeInTheDocument()
+  })
+
+  it('should render without label', () => {
+    renderWithTheme(<TextField />)
+
+    expect(screen.queryByLabelText(/label/i)).not.toBeInTheDocument()
+  })
+
+  it('should render with a placeholder', () => {
+    renderWithTheme(<TextField placeholder="placeholder" />)
+
+    expect(screen.getByPlaceholderText(/placeholder/i)).toBeInTheDocument()
+  })
+
+  it('should render with an icon', () => {
+    renderWithTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render with an icon at right', () => {
+    renderWithTheme(
+      <TextField iconPosition="right" icon={<Email data-testid="icon" />} />
+    )
+
+    expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 })
+  })
+
+  it(`should change it's value when typing`, async () => {
+    const onInput = jest.fn()
+
+    renderWithTheme(
+      <TextField onInput={onInput} label="label" labelFor="field" id="field" />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'Input text'
+
+    userEvent.type(input, text)
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInput).toHaveBeenCalledTimes(text.length)
+    })
+
+    expect(onInput).toHaveBeenCalledWith(text)
+  })
+
+  it(`should not change it's value when disabled`, async () => {
+    const onInput = jest.fn()
+
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="label"
+        labelFor="field"
+        id="field"
+        disabled
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    expect(input).toBeDisabled()
+
+    const text = 'Input text'
+
+    userEvent.type(input, text)
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text)
+    })
+
+    expect(onInput).not.toHaveBeenCalledWith()
+  })
+
+  it('should not be accessible with tab when disabled', () => {
+    renderWithTheme(
+      <TextField label="label" labelFor="field" id="field" disabled />
+    )
+
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(screen.getByRole('textbox')).not.toHaveFocus()
+  })
+
+  it('should be accessible with tab', () => {
+    renderWithTheme(<TextField label="label" labelFor="field" id="field" />)
+
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(screen.getByRole('textbox')).toHaveFocus()
+  })
+
+  it('should render with error', () => {
+    const { container } = renderWithTheme(
+      <TextField
+        icon={<Email data-testid="icon" />}
+        label="label"
+        labelFor="field"
+        error="Error message"
+      />
+    )
+
+    expect(screen.getByText('Error message')).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
