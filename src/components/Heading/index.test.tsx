eb650ebb89f20e136edf805529dd55dbc408253f
rafaelreisramos <rrramos@gmail.com>
2021-03-17 17:20:40 -0300

    feat: add huge font size support to the Heading component

--- src/components/Heading/index.test.tsx
+++ src/components/Heading/index.test.tsx
@@ -57,6 +57,18 @@ describe('<Heading />', () => {
     })
   })
 
+  it('should render a heading with a huge size', () => {
+    renderWithTheme(
+      <Heading size="huge" lineLeft>
+        Most Populars
+      </Heading>
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /most populars/i })
+    ).toHaveStyle({ 'font-size': '5.2rem' })
+  })
+
   it('should render a heading with a primary line color', () => {
     renderWithTheme(
       <Heading lineColor="primary" lineLeft lineBottom>
