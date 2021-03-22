985f99306091c9f38f94fba8961bac80a544c6f8
rafaelreisramos <rrramos@gmail.com>
2021-03-22 16:03:09 -0300

    feat: add GameDetails component
    Added GameDetails layout, styles, stories, and tests.
    Formatted date with Intl.
    Added platform icons from styled-icon/fa-brands
    Added argTypes platforms option selection in storybook to avoid
    errors when rendering icons.\Moved GameInfo to Game section in storybook.

---
+++ src/components/GameDetails/index.stories.tsx
@@ -0,0 +1,32 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './data.mock'
+
+export default {
+  title: 'Game/GameDetails',
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame,
+  argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['windows', 'linux', 'mac']
+      }
+    }
+  }
+} as Meta
+
+export const Default: Story<GameDetailsProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameDetails {...args} />
+  </div>
+)
