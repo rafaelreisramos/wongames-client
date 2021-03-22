c77cc6ddb5a47c556e40995874f4ea8900b6674b
rafaelreisramos <rrramos@gmail.com>
2021-03-22 13:22:03 -0300

    feat: add GameInfo component
    Added GameInfo component layout, styles, storybook and tests.
    Used row-reverse property from flex display to organize buttons.

---
+++ src/components/GameInfo/index.test.tsx
@@ -0,0 +1,37 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameInfo from '.'
+
+const props = {
+  title: 'Game title',
+  description: 'Game description',
+  price: '220,00'
+}
+
+describe('<GameInfo />', () => {
+  it('should render headind, description and price', () => {
+    const { container } = renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Game title/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/Game description/i)).toBeInTheDocument()
+    expect(screen.getByText(/\$220,00/)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /Add to cart/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /Whishlist/i })
+    ).toBeInTheDocument()
+  })
+})
