d0a72499945906dab3f28b2317db051fa3086221
rafaelreisramos <rrramos@gmail.com>
2021-03-23 09:17:19 -0300

    feat: add TextContent component
    The main reason for this component is apply our design styles to elements that came from html
    insertion.

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -3,7 +3,7 @@ import media from 'styled-media-query'
 
 import { HeadingProps, LineColors } from '.'
 
-const containerModifiers = {
+export const containerModifiers = {
   small: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.medium};
 
