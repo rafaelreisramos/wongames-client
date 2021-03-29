3258ca5e96961f21a79557505eebc579a813dd2d
rafaelreisramos <rrramos@gmail.com>
2021-03-29 01:10:42 -0300

    feat: add CartList component markup, styles, tests, and storybook
    Closes #50

---
+++ src/components/CartList/data.mock.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    img: 'https://source.unsplash.com/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'RS 215,00'
+  },
+  {
+    img: 'https://source.unsplash.com/151x70',
+    title: 'Borderlands 3',
+    price: 'RS 215,00'
+  }
+]
