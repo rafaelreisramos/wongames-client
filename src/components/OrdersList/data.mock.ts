aed525c6063c7f6af1af80034b0eaa225463277a
rafaelreisramos <rrramos@gmail.com>
2021-03-30 01:19:41 -0300

    feat: add OrdersList component markup, styles, tests, and storybook
    Added OrdersList component markup, styles, tests, and storybook.
    Changed GameItem to receive as props in order to make it render as a list-item (li).
    
    Closes #56

---
+++ src/components/OrdersList/data.mock.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    img: 'https://source.unsplash.com/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00',
+    downloadLink: 'https://wongames.com/game/download/gamelink1',
+    paymentInfo: {
+      flag: 'mastercard',
+      img: '/img/master-card.png',
+      number: '**** **** **** 1234',
+      purchaseDate: 'Purchase made on 03/28/2021 at 20:32'
+    }
+  },
+  {
+    img: 'https://source.unsplash.com/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00',
+    downloadLink: 'https://wongames.com/game/download/gamelink2',
+    paymentInfo: {
+      flag: 'visa',
+      img: '/img/visa.png',
+      number: '**** **** **** 2345',
+      purchaseDate: 'Purchase made on 03/28/2021 at 21:12'
+    }
+  }
+]
