331192005093c1e6c51095c85bc06038ee938971
rafaelreisramos <rrramos@gmail.com>
2021-04-05 19:05:10 -0300

    feat: add UserDropdown component
    Added UserDropdown markup, styles, tests, and storybook.
    
    Closes #67

---
+++ src/components/UserDropdown/styles.ts
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components'
+
+export const Username = styled.span`
+  ${({ theme }) => css`
+    padding: 0 ${theme.spacings.xxsmall};
+  `}
+`
+
+export const Nav = styled.nav`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    width: 26rem;
+    background: ${theme.colors.white};
+
+    a:not(:last-child) {
+      border-bottom: 0.1rem solid ${theme.colors.lightGray};
+    }
+  `}
+`
+
+export const Link = styled.a`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: ${theme.colors.black};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.small};
+    transition: background, color, ${theme.transition.default};
+
+    &:hover {
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+    }
+
+    > svg {
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+
+    > span {
+      margin-left: ${theme.spacings.xxsmall};
+    }
+  `}
+`
