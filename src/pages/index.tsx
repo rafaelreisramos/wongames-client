449c86b2577330b73358460dfe02c2baf4006e5b
rafaelreisramos <rrramos@gmail.com>
2021-04-08 18:09:15 -0300

    feat: add banners from api query
    Section banners now query an api.
    Query fragment banner.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import { initializeApollo } from 'utils/apollo'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import bannersMock from 'components/BannerSlider/data.mock'
 import gamesMock from 'components/GameCardSlider/data.mock'
 import highlightMock from 'components/Highlight/data.mock'
 
@@ -8,10 +11,30 @@ export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const {
+    data: { banners }
+  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
+    revalidate: 60,
     props: {
-      banners: bannersMock,
+      banners: banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        ...(banner.button && {
+          buttonLabel: banner.button?.label,
+          buttonLink: banner.button?.link
+        }),
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon?.text,
+          ribbonColor: banner.ribbon?.color,
+          ribbonSize: banner.ribbon?.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
