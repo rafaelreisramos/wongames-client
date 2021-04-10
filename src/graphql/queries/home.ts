cae22ad5e22a3210158ca80b8e589eb62414af20
rafaelreisramos <rrramos@gmail.com>
2021-04-09 21:57:38 -0300

    feat: add date variable to home query

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -4,22 +4,22 @@ import { GameFragment } from 'graphql/fragments/game'
 import { HighlightFragment } from 'graphql/fragments/highlight'
 
 export const QUERY_HOME = gql`
-  query QueryHome {
+  query QueryHome($date: Date!) {
     banners {
       ...BannerFragment
     }
 
     newGames: games(
-      where: { release_date_lte: "2021-04-08" }
-      sort: "release_date:asc"
+      where: { release_date_lte: $date }
+      sort: "release_date:desc"
       limit: 8
     ) {
       ...GameFragment
     }
 
     upcomingGames: games(
-      where: { release_date_gt: "2021-04-08" }
-      sort: "release_date:desc"
+      where: { release_date_gt: $date }
+      sort: "release_date:asc"
       limit: 8
     ) {
       ...GameFragment
