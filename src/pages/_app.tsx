9893c322443cf922027b6716afff9b0e688ae4a1
rafaelreisramos <rrramos@gmail.com>
2021-04-13 10:56:07 -0300

    chore: add ApolloProvider to allow queries on client side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,39 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
+import { ApolloProvider } from '@apollo/client'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import { useApollo } from 'utils/apollo'
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link
-          rel="shortcut icon"
-          type="image/png"
-          href="/img/favicon-32x32.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          href="/img/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="description" content="The best game store in the world" />
-      </Head>
+  const client = useApollo()
 
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            href="/img/favicon-32x32.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            href="/img/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="description" content="The best game store in the world" />
+        </Head>
+
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
