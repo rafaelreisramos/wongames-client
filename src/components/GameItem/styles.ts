90b7caa321e4e54a573288d0b6ee4608f973c854
rafaelreisramos <rrramos@gmail.com>
2021-04-19 21:36:22 -0300

    feat: add GameItem remove option

--- src/components/GameItem/styles.ts
+++ src/components/GameItem/styles.ts
@@ -15,6 +15,7 @@ export const Container = styled.div`
 
 export const GameContent = styled.div`
   display: flex;
+  width: 100%;
 `
 
 export const ImageBox = styled.div`
@@ -42,6 +43,7 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  width: 100%;
 `
 
 export const Title = styled.h3`
@@ -66,6 +68,13 @@ export const DownloadLink = styled.a`
   `}
 `
 
+export const Group = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
+
 export const Price = styled.p`
   ${({ theme }) => css`
     color: ${theme.colors.white};
@@ -74,6 +83,14 @@ export const Price = styled.p`
     border-radius: ${theme.border.radius};
     font-size: ${theme.font.sizes.small};
     font-weight: ${theme.font.bold};
+  `}
+`
+
+export const Remove = styled.div`
+  ${({ theme }) => css`
+    cursor: pointer;
+    color: ${theme.colors.primary};
+    font-size: ${theme.font.sizes.small};
   `}
 `
 
