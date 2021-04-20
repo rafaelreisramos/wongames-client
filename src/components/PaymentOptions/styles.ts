e978d0e1c6eaf0b67dc2c6bf7657e206d98be58b
rafaelreisramos <rrramos@gmail.com>
2021-04-19 21:43:22 -0300

    style: change PaymentOptions background color

--- src/components/PaymentOptions/styles.ts
+++ src/components/PaymentOptions/styles.ts
@@ -3,14 +3,11 @@ import styled, { css, DefaultTheme } from 'styled-components'
 import * as ButtonStyles from 'components/Button/styles'
 import tint from 'polished/lib/color/tint'
 
-export const Container = styled.div`
-  ${({ theme }) => css`
-    background: ${theme.colors.white};
-  `}
-`
+export const Container = styled.div``
 
 export const Body = styled.div`
   ${({ theme }) => css`
+    background: ${theme.colors.white};
     padding: ${theme.spacings.small};
   `}
 `
