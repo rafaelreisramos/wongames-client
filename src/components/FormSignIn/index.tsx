b88e2dceecd552be4c7d3bfebb0e74d7ab0ac9aa
rafaelreisramos <7602616+rafaelreisramos@users.noreply.github.com>
2021-04-26 10:55:31 -0300

    feat: form validation (#108)
    * feat: add form validations utils
    * feat: add FormSignIn/Up validation (Closes #107)

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -2,21 +2,28 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { signIn } from 'next-auth/client'
 import { FormEvent, useState } from 'react'
-import { Email, Lock } from '@styled-icons/material-outlined'
+import { Email, Lock, ErrorOutline } from '@styled-icons/material-outlined'
+
+import { FieldErrors, SignInValues, signInValidate } from 'utils/validations'
 
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-import { FormContainer, FormLink, FormLoading } from 'components/Form'
+import {
+  FormContainer,
+  FormError,
+  FormLink,
+  FormLoading
+} from 'components/Form'
 
 import * as S from './styles'
 
-export type SignIn = {
-  email: string
-  password: string
-}
-
 const FormSignIn = () => {
-  const [values, setValues] = useState<SignIn>({ email: '', password: '' })
+  const [values, setValues] = useState<SignInValues>({
+    email: '',
+    password: ''
+  })
+  const [fieldError, setFieldError] = useState<FieldErrors>({})
+  const [formError, setFormError] = useState('')
   const [loading, setLoading] = useState(false)
   const { push, query } = useRouter()
 
@@ -24,9 +31,22 @@ const FormSignIn = () => {
     setValues((s) => ({ ...s, [field]: value }))
   }
 
+  const handleFocus = (field: string) => {
+    setFieldError((s) => ({ ...s, [field]: '' }))
+  }
+
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault()
     setLoading(true)
+    setFormError('')
+
+    const errors = signInValidate(values)
+
+    if (Object.keys(errors).length) {
+      setLoading(false)
+      setFieldError(errors)
+      return
+    }
 
     const result = await signIn('credentials', {
       ...values,
@@ -39,26 +59,36 @@ const FormSignIn = () => {
     }
 
     setLoading(false)
-    console.log('email ou senha inválida')
+    setFormError('username or password are invalid')
   }
 
   return (
     <FormContainer>
+      {!!formError && (
+        <FormError>
+          <ErrorOutline />
+          {formError}
+        </FormError>
+      )}
       <form onSubmit={handleSubmit}>
         <TextField
           name="email"
           type="email"
+          error={fieldError?.email}
           placeholder="E-mail"
           onInputChange={(v) => handleInput('email', v)}
           icon={<Email />}
+          onFocus={() => handleFocus('email')}
         />
 
         <TextField
           name="password"
           type="password"
+          error={fieldError?.password}
           placeholder="Password"
           onInputChange={(v) => handleInput('password', v)}
           icon={<Lock />}
+          onFocus={() => handleFocus('password')}
         />
 
         <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
---
+++ src/utils/validations/index.test.ts
@@ -0,0 +1,90 @@
+import { signInValidate, signUpValidate } from '.'
+
+describe('validations', () => {
+  describe('signInValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = { email: '', password: '' }
+
+      expect(signInValidate(values)).toMatchObject({
+        email: '"email" is not allowed to be empty',
+        password: '"password" is not allowed to be empty'
+      })
+    })
+
+    it('should return invalid email error', () => {
+      const values = { email: 'invalid', password: '12345678' }
+
+      expect(signInValidate(values)).toMatchInlineSnapshot(`
+        Object {
+          "email": "\\"email\\" must be a valid email",
+        }
+      `)
+    })
+
+    it('should return min length password error', () => {
+      const values = { email: '', password: '1234' }
+
+      expect(signInValidate(values).password).toMatchInlineSnapshot(
+        `"\\"password\\" length must be at least 8 characters long"`
+      )
+    })
+  })
+
+  describe('signUpValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = {
+        username: '',
+        email: '',
+        password: ''
+      }
+
+      expect(signUpValidate(values)).toMatchObject({
+        username: expect.any(String),
+        email: expect.any(String),
+        password: expect.any(String),
+        confirm_password: expect.any(String)
+      })
+    })
+
+    it('should return min length username error', () => {
+      const values = {
+        username: 'Joe',
+        email: '',
+        password: ''
+      }
+
+      expect(signUpValidate(values).username).toMatchInlineSnapshot(
+        `"\\"username\\" length must be at least 5 characters long"`
+      )
+    })
+
+    it('should return invalid email error', () => {
+      const values = { username: '', email: 'invalid', password: '' }
+
+      expect(signUpValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+
+    it('should return min length password error', () => {
+      const values = { username: '', email: '', password: '1234' }
+
+      expect(signUpValidate(values).password).toMatchInlineSnapshot(
+        `"\\"password\\" length must be at least 8 characters long"`
+      )
+    })
+
+    it('should return error if password does not match with confirm password', () => {
+      const values = {
+        username: 'John Doe',
+        email: 'john.doe@gmail.com',
+        password: '12345678',
+        confirm_password: '87654321'
+      }
+
+      expect(signUpValidate(values).confirm_password).toMatchInlineSnapshot(
+        `"confirm password does not match with password"`
+      )
+    })
+  })
+})
