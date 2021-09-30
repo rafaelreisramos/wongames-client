851a524e9ed3fe72ec27fad909038c901e8122b6
rafaelreisramos <rrramos@gmail.com>
2021-09-30 08:30:15 -0300

    test: add and config testing-library cypress plugin

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,7 +1,17 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should got to Google homepage', () => {
+  it.skip('should got to Google homepage', () => {
     cy.google()
+  })
+
+  it('should change light/dark theme on willian justen site', () => {
+    cy.visit('https://willianjusten.com.br')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.light').should('exist')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.dark').should('exist')
   })
 })
