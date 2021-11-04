1ea662e0ebef27f80b07be670af0c85204bcb9ed
rafaelreisramos <rrramos@gmail.com>
2021-11-03 21:00:57 -0300

    test(sign in): add sign in/out tests

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -3,7 +3,7 @@
 import { createUser } from '../support/generate'
 
 describe('User', () => {
-  it('should be able to sign up', () => {
+  it.skip('should be able to sign up', () => {
     cy.visit('/sign-up')
 
     const user = createUser()
@@ -11,5 +11,19 @@ describe('User', () => {
     
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
+  })
+
+  it('should be able to sign in and sign out', () => {
+    cy.visit('/sign-in')
+
+    cy.findByPlaceholderText(/e-mail/i).type('e2e@wongames.com')
+    cy.findByPlaceholderText(/password/i).type('12345678')
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.contains(/cypress/i, { timeout: 8000 }).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByText(/cypress/i).should('not.exist')
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
   })
 })
