e88626588f29e304efb25585c790f557426af327
rafaelreisramos <rrramos@gmail.com>
2021-11-19 16:16:46 -0300

    feat: add cloudinary image host support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
@@ -6,7 +7,7 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development'
   },
   images: {
-    domains: ['localhost']
+    domains: ['localhost', 'res.cloudinary.com']
   },
   webpack5: true
 })
